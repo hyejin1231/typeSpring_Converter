8ae15420618f9afea5944cab537e9645e368c639
hyejin1231 <phj981231@naver.com>
2022-01-17 11:30:45 +0900

    20220117 Spring Converter #4 Spring WebConfig에 직접 만든 컨버터들 등록하고 확인해보기

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.ipPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,14 @@ public class HelloController {
     @GetMapping("/hello-v2")
     public String helloV2(@RequestParam Integer data) {
         System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam ipPort port) {
+        System.out.println("port ip= " + port.getIp());
+        System.out.println("port.getPort() = " + port.getPort());
+
         return "ok";
     }
 }
