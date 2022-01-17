fe7783154f70c406098ce8842c79777a000c8d83
hyejin1231 <phj981231@naver.com>
2022-01-17 13:03:26 +0900

    20220117 Spring Converter #6 Formatter

---
+++ src/test/java/hello/typeconverter/formatter/MyNumberFormatterTest.java
@@ -0,0 +1,27 @@
+package hello.typeconverter.formatter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyNumberFormatterTest {
+
+    MyNumberFormatter formatter = new MyNumberFormatter();
+
+    @Test
+    void parse() throws ParseException {
+        Number result = formatter.parse("1,000", Locale.KOREA);
+        assertThat(result).isEqualTo(1000L); // Number는 추상이기 때문에 구체적인 타입으로 지정해줘야해서 Long타입 지정해줌!
+    }
+
+    @Test
+    void print() {
+        String result = formatter.print(1000, Locale.KOREA);
+        assertThat(result).isEqualTo("1,000");
+    }
+}
